6aee8ac4ff422713627051a2dd45ea2f3bddff77
AhmedAdly20 <ahmedadly132@gmail.com>
2020-11-21 01:29:37 +0200

    Select Image From Gallery & Capture One From The Camera

--- lib/pages/UploadPage.dart
+++ lib/pages/UploadPage.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UploadPage extends StatefulWidget {
   @override
@@ -6,8 +8,79 @@ class UploadPage extends StatefulWidget {
 }
 
 class _UploadPageState extends State<UploadPage> {
+
+  File file;
+
+  captureImageWithCamera() async{
+    Navigator.pop(context);
+    File imageFile = await ImagePicker.pickImage(
+      source: ImageSource.camera,
+      maxHeight: 688,
+      maxWidth: 970,
+    );
+    setState(() {
+      this.file = imageFile;
+    });
+  }
+
+  pickImageFromGallery() async{
+    Navigator.pop(context);
+    File imageFile = await ImagePicker.pickImage(
+      source: ImageSource.gallery,
+    );
+    setState(() {
+      this.file = imageFile;
+    });
+  }
+
+  takeImage(acontext){
+    return showDialog(
+      context: acontext,
+      builder: (context) {
+        return SimpleDialog(
+          title: Text('New Post', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+          children: [
+            SimpleDialogOption(
+              child: Text('Capture Image with Camera',style: TextStyle(color: Colors.white)),
+              onPressed: captureImageWithCamera,
+            ),
+            SimpleDialogOption(
+              child: Text('Select Image From Gallery',style: TextStyle(color: Colors.white)),
+              onPressed: pickImageFromGallery,
+            ),
+            SimpleDialogOption(
+              child: Text('Cancel',style: TextStyle(color: Colors.white)),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  displayUploadScreen(){
+    return Container(
+      color: Theme.of(context).accentColor.withOpacity(0.5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.add_photo_alternate, color: Colors.grey, size: 200.0),
+          Padding(
+            padding: EdgeInsets.only(top:20.0),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(9.0)),
+              child: Text('Upload Image', style: TextStyle(color: Colors.white, fontSize: 20.0),),
+              color: Colors.green,
+              onPressed: () => takeImage(context),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Here goes Upload Page.",style: TextStyle(color: Colors.white),);
+    return displayUploadScreen();
   }
 }
